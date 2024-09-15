c478a30e6c84639bdf2761f5191ea3ed9a304699
Pavan Kumar <115294457+pavankumart18@users.noreply.github.com>
2024-09-15 14:18:44 +0530

    Update FireBaseConfigs.js

--- config/FireBaseConfigs.js
+++ config/FireBaseConfigs.js
@@ -10,15 +10,7 @@ import { getFirestore, collection, addDoc, deleteDoc, doc, onSnapshot ,orderBy,q
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAsHazxpZZM4uQXSJBXhVMYMQkx1SfZWek",
-  authDomain: "mylife-ee495.firebaseapp.com",
-  projectId: "mylife-ee495",
-  storageBucket: "mylife-ee495.appspot.com",
-  messagingSenderId: "1001161614415",
-  appId: "1:1001161614415:web:972c6eb88f26050a49f63a",
-  measurementId: "G-2MT1PRKX71"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
